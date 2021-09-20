8c2f115bfe1b878fa9e8267b4eb4a0a1c763a780
wilson <wilson.m.freire1@gmail.com>
2021-09-20 11:01:20 -0300

    Conectando ao Twiter e buscando lista de usuaris

--- recent_search.py
+++ recent_search.py
@@ -19,9 +19,14 @@ def create_url():
     # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
     # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
     # source, text, and withheld
-    tweet_fields = "tweet.fields=author_id"
-    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
-        query, tweet_fields
+    ##usando API p/buscar informacoes pontuais
+    tweet_fields = "tweet.fields=author_id, conversation_id, created_at, id, in_reply_to_user_id, public_metrics, text"
+    ##expandido os usuarios, autor, id, quando foi criado
+    user_fields = "expansions=author_id&user.fields=id, name, username, created_at"
+    ##buscando data inicial e final
+    filters = "start_time=2021-02-15T00:00:00Z&end_time=2021-02-19T00:00:00Z"
+    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}&{}&{}".format(
+        query, tweet_fields, user_fields, filters
     )
     return url
 
